df0e6c8de0f8a6a07c3673dd8e0ff405974b0fbc
jlaron230 <jlaron230@hotmail.fr>
2025-04-11 00:53:26 +0200

    flex navbar

--- frontend/src/components/Image/Logo.jsx
+++ frontend/src/components/Image/Logo.jsx
@@ -2,7 +2,7 @@ import logo from "@assets/image/LogoGestock.png";
 const Logo = () => {
     return (
         <>
-            <img src={logo} className="w-35 mr-2" alt="Logo Gestock"/>
+            <img src={logo} className="w-35" alt="Logo Gestock"/>
         </>
     )
 }
