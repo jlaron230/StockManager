d3ffe4235b8c22fddfb903b3baf34c0e34fc9a44
GAVINO Jerome <jerome.GAVINO@univ-amu.fr>
2025-04-11 17:10:01 +0200

    add/home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from "react";
 import Counter from "@components/Counter";
 import Navbar from "@components/Navbar/Navbar";
+import HomeComponent from "@components/Home/HomeComponent";
 
 const Home = () => {
     return (
         <div>
-         <Home />
+         <HomeComponent />
         </div>
     )
 }
