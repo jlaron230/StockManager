c3552d6dbf9ed07943df63b3b785ba29558a2732
Mael1to <mael.comf@gmail.com>
2025-05-14 11:35:47 +0200

    Update test-fcm.js
    test envoie notif avec liste des produit en état critique

--- test-fcm.js
+++ test-fcm.js
@@ -1,5 +1,8 @@
 const admin = require("./backend/config/firebase");
 
+//test notif cd backend
+//node test-stock.js
+
 console.log("🧪 Vérification FCM...");
 console.log("Type admin.messaging:", typeof admin.messaging);
 console.log("Fonctions disponibles:", Object.keys(admin.messaging()));
